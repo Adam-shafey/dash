9586b3e9fbdb3586fb00840e0f21b4f9fa530ab1
Adam-shafey <abdoshafey1@gmail.com>
2017-12-10 16:51:26 -0500

    Added some features to establish a master branch baseline
    -Added the functionality to add and remove the basic placeholder card
    -Investigated the possibility and added as comment horizontal compression(not applied) as well as vertical compression(applied)

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
-import Body from './Body.js';
+import AddRemoveLayout from './AddRemoveLayout.js';
+
 
 // Your widget. Just another react component.
-import ReactGridLayout from 'react-grid-layout';
 // Default styles.
-import '../node_modules/react-grid-layout/css/styles.css';
-import '../node_modules/react-resizable/css/styles.css';
 
 //grid from: https://github.com/strml/react-grid-layout
 //cards from React Material UI
@@ -18,23 +15,15 @@ class App extends Component {
     super(props);
     // Set initial state
 
-    this.state = {  
-      cards: [{name: "lol"}, {name: "lolzy"}],      
-    };
+    this.state = {
+
+    }
   }
+
   render() {
-    var layout = [
-      {i: 'a', x: 0, y: 0, w: 5, h: 7},
-      {i: 'b', x: 0, y: 0, w: 5, h: 5, minW: 2, maxW: 4},
-      {i: 'c', x: 0, y: 0, w: 5, h: 5}
-    ];
     return (
       <MuiThemeProvider className="App">
-      <ReactGridLayout className="layout" layout={layout} cols={12} rowHeight={30} width={1200}>
-        <div key="a"><Body cards={this.state.cards} /></div>
-        <div key="b">b</div>
-        <div key="c">c</div>
-      </ReactGridLayout>
+      <AddRemoveLayout/>
       </MuiThemeProvider>
     );
   }
